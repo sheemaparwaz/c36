d03df41874d41d488cff17fcf0df6325e0689e22
sheema <sheemaparwaz@gmail.com>
2020-11-25 21:02:53 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor(){}
+
+    getCount(){
+        var playerCountRef = database.ref("plaerCount");
+        playerCountRef.on("value", function(data){
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count){
+        database.ref("/").update({
+            playerCount: count
+        })
+    }
+
+    update(name){
+        var playerIndex = "player" + playerCount;
+        database.ref(playerIndex).set({
+            name:name
+        })
+    }
+}
